3839dc84da9d08d9c7c079c0776b09d98300235b
Chris Seufert <chris@modd.com.au>
2023-07-04 12:58:31 +1000

    Added NetSuiteRestAPI to library

--- generator/base/Loader.php
+++ generator/base/Loader.php
@@ -1,47 +1,41 @@
 <?php
 declare(strict_types=1);
 
-$base = dirname(__FILE__);
-include_once($base . "/RequestAbstract.php");
-include_once($base . "/SuiteQL.php");
-include_once($base . "/NetsuiteException.php");
-foreach (glob("$base/NSRecord/*.php") as $filename) {
-    include_once $filename;
-}
+namespace NetSuiteRestAPI;
 
 class Loader
 {
-    /**
-     * Loads config from config.json
-     *
-     * @param stdClass|null $configData Load config from an external source
-     * @return array
-     */
-    public function getConfig($configData = null)
-    {
-        static $config = null;
-        if ($config === null) {
-            $config = $configData !== null
-                ? $configData
-                : json_decode(
-                    file_get_contents(
-                        sprintf(
-                            '%s/config.json',
-                            dirname(__FILE__)
-                        )
-                    )
-                );
-        }
-        $accountId = str_replace('_', '-', strtolower($config->ACCOUNT_ID));
+	/**
+	 * Loads config from config.json
+	 *
+	 * @param stdClass|null $configData Load config from an external source
+	 * @return array
+	 */
+	public function getConfig($configData = null)
+	{
+		static $config = null;
+		if ($config === null) {
+			$config = $configData !== null
+				? $configData
+				: json_decode(
+					file_get_contents(
+						sprintf(
+							'%s/config.json',
+							dirname(__FILE__)
+						)
+					)
+				);
+		}
+		$accountId = str_replace('_', '-', strtolower($config->ACCOUNT_ID));
 
-        return [
-            'account_id'        => $accountId,
-            'realm'             => str_replace('-', '_', strtoupper($config->ACCOUNT_ID)),
-            'consumer_key'      => $config->CONSUMER_KEY,
-            'consumer_secret'   => $config->CONSUMER_SECRET,
-            'token_id'          => $config->TOKEN_ID,
-            'token_secret'      => $config->TOKEN_SECRET,
-            'api_source'        => $config->API_SOURCE,
-        ];
-    }
+		return [
+			'account_id' => $accountId,
+			'realm' => str_replace('-', '_', strtoupper($config->ACCOUNT_ID)),
+			'consumer_key' => $config->CONSUMER_KEY,
+			'consumer_secret' => $config->CONSUMER_SECRET,
+			'token_id' => $config->TOKEN_ID,
+			'token_secret' => $config->TOKEN_SECRET,
+			'api_source' => $config->API_SOURCE,
+		];
+	}
 }
